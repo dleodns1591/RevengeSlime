889ffcf736a11c56a017d46fa88fcfd429e65bda
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-09-22 20:47:19 +0900

    맵 배치
    맵 배치

---
+++ Re_SlimeRevenge_Project/Assets/Script/Title.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+public class Title : MonoBehaviour
+{
+    public SpriteRenderer TouchToStart;
+
+    void Start()
+    {
+        StartCoroutine(Text_Blinking_01());
+    }
+
+    void Update()
+    {
+        Next_Scene();
+    }
+
+    public void Next_Scene()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TouchToStart.DOPause();
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    public IEnumerator Text_Blinking_01()
+    {
+        TouchToStart.DOFade(0.5f, 1);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(Text_Blinking_02());
+    }
+
+    public IEnumerator Text_Blinking_02()
+    {
+        TouchToStart.DOFade(0.5f, 1);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(Text_Blinking_01());
+    }
+
+}
